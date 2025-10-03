60173600c045d357db34bc261975355bea1b966e
Abhinav Anand <abhinavanand072@gmail.com>
2025-10-03 13:14:23 +0530

    installed tailwindcss

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Login = () => {
     return (
-        <div style={{padding: '20px'}}>
+        <div style={{padding: '20px', maxWidth: '400px', margin: '50px auto', border: '1px solid #ccc', borderRadius: '8px'}}>
             <h2>User Login</h2>
         </div>
     )
